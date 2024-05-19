31f508735cb8ddb7f2548e2647b9c190b75e95d9
akshay <akshaytanwar1701@gmail.com>
2024-05-19 21:25:41 +0530

    bug fix

--- src/component/UI/Product/Products.js
+++ src/component/UI/Product/Products.js
@@ -25,8 +25,8 @@ const Products = () => {
           <div className="col-md-3">
             <PriceFilter CategoryId={id} />
           </div>
-          <div className="col-md-9">
-            <div className="row row-cols-sm-2 row-cols-md-4 g-4">
+          <div className="container col-9">
+            <div className="row row-cols-2 row-cols-lg-4 g-1 g-lg-3">
               {!key
                 ? Product.map((element) => {
                     return (
@@ -44,37 +44,6 @@ const Products = () => {
                       </div>
                     );
                   })}
-            </div>
-            <div className="container d-flex justify-content-center">
-              <nav aria-label="Page navigation example">
-                <ul className="pagination">
-                  <li className="page-item">
-                    <a className="page-link" href="#" aria-label="Previous">
-                      <span aria-hidden="true">&laquo;</span>
-                    </a>
-                  </li>
-                  <li className="page-item">
-                    <a className="page-link" href="#">
-                      1
-                    </a>
-                  </li>
-                  <li className="page-item">
-                    <a className="page-link" href="#">
-                      2
-                    </a>
-                  </li>
-                  <li className="page-item">
-                    <a className="page-link" href="#">
-                      3
-                    </a>
-                  </li>
-                  <li className="page-item">
-                    <a className="page-link" href="#" aria-label="Next">
-                      <span aria-hidden="true">&raquo;</span>
-                    </a>
-                  </li>
-                </ul>
-              </nav>
             </div>
           </div>
         </div>
