160330338b4a5de64d456a5696c6a1d457937079
akshay <akshaytanwar1701@gmail.com>
2024-06-06 16:22:10 +0530

    changing the backend url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  APIUrl: "https://ecommerce-website-using-mern-stack.onrender.com",
+  APIUrl: "https://digishopbackend.onrender.com",
   fontFamily: "Trebuchet MS sans-serif",
   // APIUrl: "https://backend--digishopakshay.netlify.app",
   // APIUrl: "http://localhost:5000",
